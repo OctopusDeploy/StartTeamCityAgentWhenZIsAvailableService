6d08e44e846bf1b18da612033890c877326ca0ea
Matt Richardson <matt@redgumtech.com.au>
2016-11-09 06:08:44 +1100

    Make build script fail teamcity build

--- build.ps1
+++ build.ps1
@@ -1,15 +1,14 @@
 . dotnet restore
+if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 . dotnet build
+if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 . dotnet publish -o publish
-if($LASTEXITCODE -ne 0) {
-    throw
-}
+if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-if(-not(Test-Path Merged)) {
+if (-not(Test-Path Merged)) {
     mkdir Merged
 }
+
 $dlls = Get-Item .\publish\*.dll
 .\ILRepack.exe .\publish\StartTeamCityAgentWhenZIsAvailableService.exe $dlls /out:Merged\StartTeamCityAgentWhenZIsAvailableService.exe
-if($LASTEXITCODE -ne 0) {
-    throw
-}
+exit $LASTEXITCODE
